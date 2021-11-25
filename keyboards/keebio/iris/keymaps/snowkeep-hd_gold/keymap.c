0c557a3c59d56fa5a3c8d5f99981905d8013b40f
Craig Joly <craig@snowkeep.ca>
2021-11-25 14:49:37 -0500

    couple of changes informed by the Atreus

--- keyboards/keebio/iris/keymaps/snowkeep-hd_gold/keymap.c
+++ keyboards/keebio/iris/keymaps/snowkeep-hd_gold/keymap.c
@@ -37,13 +37,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      XXXXXXX, XXXXXXX, XXXXXXX, RGB_VAI, RGB_SAI, RGB_HUI,                            RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX, KC_PAUS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_TOG, KC_QUOT, KC_LT,   KC_GT,   KC_DQT,  KC_DOT,                             KC_AMPR, KC_UNDS, KC_LBRC, KC_RBRC, KC_PERC, XXXXXXX,
+     RGB_TOG, KC_GRV,  KC_LT,   KC_GT,   KC_DQT,  KC_DOT,                             KC_AMPR, KC_UNDS, KC_LBRC, KC_RBRC, KC_PERC, XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      RGB_MOD, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL,  KC_HASH,                            KC_PIPE, KC_COLN, KC_LPRN, KC_RPRN, KC_QUES, XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_CIRC, KC_SLSH, KC_ASTR, KC_BSLS, M_UPDIR, _______,          _______, KC_GRV,  KC_DLR,  KC_LCBR, KC_RCBR, KC_AMPR, _______,
+     _______, KC_CIRC, KC_SLSH, KC_ASTR, KC_BSLS, M_UPDIR, _______,          _______, KC_TILD, KC_DLR,  KC_LCBR, KC_RCBR, XXXXXXX, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    NAV,     KC_ENT,  KC_SPC,                    XXXXXXX, SYM,     FUNC
+                                    _______, _______, _______,                   XXXXXXX, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -51,13 +51,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RESET,   XXXXXXX, KC_PGUP, KC_UP,   KC_PGDN, KC_LPRN,                            KC_RPRN, KC_7,    KC_8,    KC_9,    KC_MINS, KC_PGDN,
+     _______, XXXXXXX, KC_HOME, KC_UP,   KC_END,  KC_PGUP,                            KC_UP,   KC_7,    KC_8,    KC_9,    KC_MINS, XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_DEL,  XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_LBRC,                            KC_RBRC, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_HOME,
+     KC_DEL,  XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                            KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     BL_STEP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_LPRN,          KC_RPRN, KC_RCBR, KC_1,    KC_2,    KC_3,    KC_EQL,  KC_END,
+     BL_STEP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, KC_1,    KC_2,    KC_3,    KC_EQL,  _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, KC_DEL,                    KC_DEL,  KC_0,    KC_DOT
+                                    _______, _______, KC_DEL,                    KC_BSPC, KC_0,    KC_DOT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -65,11 +65,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_TOG, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+     RESET,   XXXXXXX, KC_MPRV, KC_VOLU, KC_MNXT, XXXXXXX,                            XXXXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_MOD, KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, KC_UNDS,                            KC_EQL,  KC_HOME, RGB_HUI, RGB_SAI, RGB_VAI, _______,
+     XXXXXXX, XXXXXXX, KC_MPLY, KC_VOLD, XXXXXXX, XXXXXXX,                            XXXXXXX, KC_F4,   KC_F5,   KC_F6,   KC_F11,  _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_MINS, _______,          _______, KC_PLUS, KC_END,  RGB_HUD, RGB_SAD, RGB_VAD, _______,
+     XXXXXXX, XXXXXXX, KC_MSTP, KC_MUTE, XXXXXXX, XXXXXXX, _______,          _______, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F12,  _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
